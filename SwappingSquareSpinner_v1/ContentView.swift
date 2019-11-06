6e6c696526c1348cc3e1ae13dc41e2eee6494e89
Viettasc <viettasc@gmail.com>
2019-11-06 16:28:55 +0700

    Initial Commit

---
+++ SwappingSquareSpinner_v1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwappingSquareSpinner_v1
+//
+//  Created by Viettasc on 11/6/19.
+//  Copyright © 2019 Viettasc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
